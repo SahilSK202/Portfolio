ff92b1087e7ebbc4d98f6a38a43fd7804ae08cec
sahil kavitake <sahir.kavitake202@gmail.com>
2023-07-03 01:21:32 +0530

    Updated Data

--- main.js
+++ main.js
@@ -33,18 +33,34 @@ $(document).ready(function () {
     $(".menu-btn i").toggleClass("active");
   });
 
+  function getExperienceFromDate(startDate) {
+    const currentDate = new Date();
+    const startYear = startDate.getFullYear();
+    const startMonth = startDate.getMonth();
+    const startDay = startDate.getDate();
+
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth();
+    const currentDay = currentDate.getDate();
+
+    let yearsOfExperience = currentYear - startYear;
+
+    const decimalYearsOfExperience =
+      yearsOfExperience + (currentMonth - startMonth) / 12;
+    return decimalYearsOfExperience.toFixed(1);
+  }
+
+  // Usage example:
+  const startDate = new Date("2021-07-01");
+  const experience = getExperienceFromDate(startDate);
+  $("#exp-yrs").html(experience);
+  console.log(experience);
+
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: [
-      "Data Science",
-      "Machine Learning",
-      "Data Analysis",
-      "Deep Learning",
-      "A.I.",
-      "Reinforcement Learning",
-    ],
-    typeSpeed: 100,
-    backSpeed: 60,
+    strings: ["Data Analysis | Web Development | Machine Learning"],
+    typeSpeed: 50,
+    backSpeed: 1,
     loop: true,
   });
 
@@ -58,7 +74,7 @@ $(document).ready(function () {
       "Reinforcement Learning",
     ],
     typeSpeed: 100,
-    backSpeed: 60,
+    backSpeed: 100,
     loop: true,
   });
 
